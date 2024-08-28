f620a26bff57875d352b3d4cdd772a393bdfc269
Pratham Asthana <prathamasthana04@gmail.com>
2024-08-28 14:03:31 +0530

    update

---
+++ functional files/path.py
@@ -0,0 +1,2 @@
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
--- path.py
+++
@@ -1,2 +0,0 @@
-import pytesseract
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
